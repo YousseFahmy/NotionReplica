00a2da455849d4205fcbee084c04d1f166d691f6
Karim Ashraf <karimshura@gmail.com>
2024-05-08 19:34:47 +0300

    Initialized app for the first time

---
+++ src/main/java/com/example/SageScalable/SageScalableApplication.java
@@ -0,0 +1,13 @@
+package com.example.SageScalable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SageScalableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SageScalableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SageScalable/SageScalableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SageScalable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SageScalableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
