66945cb5b0b5351133cf41e4b73280664044e2fe
Karim Wahdan <karim.walid.wahdan@gmail.com>
2024-05-10 19:49:52 +0300

    isa

--- src/test/java/com/notionreplica/notion/NotionApplicationTests.java
+++ src/test/java/com/notionreplica/notion/NotionApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.notionreplica.notion;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NotesAppApplicationTests {
+class NotionApplicationTests {
 
 	@Test
 	void contextLoads() {
